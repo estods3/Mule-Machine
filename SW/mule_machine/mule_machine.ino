8db4d08e93333283780ca1e8de41ed6631e10879
estods3 <estods76@gmail.com>
2022-02-08 21:27:04 -0500

    implemented new calibration with pots. not yet tested on HW

--- SW/mule_machine/mule_machine.ino
+++ SW/mule_machine/mule_machine.ino
@@ -1,39 +1,53 @@
 /*
  Mule Machine
-
+ @Author: Evan Stoddart 
+ Email: estods76@gmail.com Github: estods3
  Constants: Pinout and Calibration
  Setup: initialize pins, perform a bootup animation
- Loop: Wait for lever to be pulled. check conditions + make drink
+ Loop: Wait for cup to be placed on machine. check conditions + make drink.
 */
 
 // PINOUT DEFINITION
-const int ledOrange_Vodka = 4;
-const int ledYellow_GingerBeer = 5;
-const int ledGreen_Lime =  6;
+//inputs
+bool cup_is_present = false;
+const int cup_present_buttonPin = 5; //PD5, physical pin 11
+const int reset_button_pin = 6; //PD6, physical pin 12
+const int tuner_strength_pin = A4; //PC4, physical pin 27
+const int tuner_sour_pin = A5; //PC5, physical pin 28
+//outputs
+const int ledOrange_Vodka = 2; // PD2, physical pin 4
+const int ledYellow_GingerBeer = 3; // PD3, physical pin 5
+const int ledGreen_Lime =  4; // PD4, physical pin 6
+const int pump_Vodka = A1; //PC1,A1, physical pin 24
+const int pump_GingerBeer = A2; //PC2,A2, physical pin 25
+const int pump_Lime =  A3; //PC3,A3, physical pin 26
 
-const int pump_Vodka = 9;
-const int pump_GingerBeer = 10;
-const int pump_Lime =  11;
+//ANIMATION DELAYS
+const int bootup_blink_duration = 100;
+const int standby_blink_duration = 15;
+const int standby_animation_interval = 3000;
+const int drink_starting_blink_duration = 100;
 
-bool lever_is_pulled = false;
-const int lever_buttonPin = 2;
+//PUMP CURRENT SPIKE DELAY
+// prevent pumps drawing too much current from Arduino when turning on all at once.
+const int current_spike_delay = 10;
 
-bool cupSensor_cupPresent = false;
-const int cupSensor_buttonPin = 3;
+
 
 //TODO numDrinksTilEmpty
 
 // MULE CALIBRATION
-const int calibration_time_pour_duration = 1000; //1 second pour at a time
-const double calibration_ratio_Lime = 0.1; // 0.0625 Cups per serving --> 0.5/0.75 % of calibration_time_pour_duration spent pouring lime juice
-const double calibration_ratio_GingerBeer = 0.7; // 0.5 Cups per serving --> 0.5/0.75 % of calibration_time_pour_duration spent pouring ginger beer
-const double calibration_ratio_Vodka = 0.2; // 0.1875 Cups per serving % of calibration_time_pour_duration spent pouring vodka
+int value; //save analog value
+double calibration_ratio_Lime;
+double calibration_ratio_GingerBeer;
+double calibration_ratio_Vodka;
 
 // FUNCTION: setup()
 void setup() {
-  // Setup the pins as input/output
-  pinMode(lever_buttonPin, INPUT_PULLUP);
-  pinMode(cupSensor_buttonPin, INPUT_PULLUP);
+  // set the input pins
+  pinMode(tuner_strength_pin, INPUT);
+  pinMode(tuner_sour_pin, INPUT);
+  pinMode(cup_present_buttonPin, INPUT_PULLUP);
   // set the LEDs and Pumps as output
   pinMode(ledOrange_Vodka, OUTPUT);
   pinMode(ledYellow_GingerBeer, OUTPUT);
@@ -49,39 +63,38 @@ void setup() {
   digitalWrite(pump_Lime, LOW);
   
   // Perform an LED animation to let you know the machine is up and runnning
-  delay(1000);
+  delay(bootup_blink_duration);
   digitalWrite(ledOrange_Vodka, HIGH);
-  delay(100);
+  delay(bootup_blink_duration);
   digitalWrite(ledOrange_Vodka, LOW);
   digitalWrite(ledYellow_GingerBeer, HIGH);
-  delay(100);
+  delay(bootup_blink_duration);
   digitalWrite(ledYellow_GingerBeer, LOW);
   digitalWrite(ledGreen_Lime, HIGH);
-  delay(100);
+  delay(bootup_blink_duration);
   digitalWrite(ledGreen_Lime, LOW);
   digitalWrite(ledYellow_GingerBeer, HIGH);
-  delay(100);
+  delay(bootup_blink_duration);
   digitalWrite(ledYellow_GingerBeer, LOW);
   digitalWrite(ledOrange_Vodka, HIGH);
-  delay(100);
+  delay(bootup_blink_duration);
   digitalWrite(ledOrange_Vodka, LOW);
   digitalWrite(ledYellow_GingerBeer, HIGH);
-  delay(100);
+  delay(bootup_blink_duration);
   digitalWrite(ledYellow_GingerBeer, LOW);
   digitalWrite(ledGreen_Lime, HIGH);
-  delay(100);
+  delay(bootup_blink_duration);
   digitalWrite(ledGreen_Lime, LOW);
   digitalWrite(ledYellow_GingerBeer, HIGH);
-  delay(100);
+  delay(bootup_blink_duration);
   digitalWrite(ledYellow_GingerBeer, LOW);
   digitalWrite(ledOrange_Vodka, HIGH);
-  delay(100);
+  delay(bootup_blink_duration);
   digitalWrite(ledOrange_Vodka, LOW);
   digitalWrite(ledYellow_GingerBeer, LOW);
   digitalWrite(ledGreen_Lime, LOW);
 
-  lever_is_pulled = false;
-  cupSensor_cupPresent = false;
+  cup_is_present = false;
 }
 
 
@@ -89,86 +102,112 @@ void setup() {
 void loop() {
   // Perform a short animation every 3 seconds to let you know machine is alive
   digitalWrite(ledOrange_Vodka, HIGH);
-  delay(25);
+  delay(standby_blink_duration);
   digitalWrite(ledOrange_Vodka, LOW);
   digitalWrite(ledYellow_GingerBeer, HIGH);
-  delay(25);
+  delay(standby_blink_duration);
   digitalWrite(ledYellow_GingerBeer, LOW);
   digitalWrite(ledGreen_Lime, HIGH);
-  delay(25);
+  delay(standby_blink_duration);
   digitalWrite(ledGreen_Lime, LOW);
-  delay(3000);
+  delay(standby_animation_interval);
   digitalWrite(ledOrange_Vodka, LOW);
   digitalWrite(ledYellow_GingerBeer, LOW);
   digitalWrite(ledGreen_Lime, LOW);
 
   //TODO if(numDrinksTilEmpty < 3) flash RED LED
   
-  // Wait for Mule Lever (Button) Press
-  // NOTE LOW=="Pulled" as the pull up resistor keeps the button value HIGH by default and pushing
+  // Wait for Copper Cup (Button Press)
+  // NOTE LOW=="CUP PRESENT" as the pull up resistor keeps the button value HIGH by default and pushing
   // the button creates a LOW signal.
   // When button pressed: call functions for Vodka, GB, and Lime to pour until button is depressed.
-  lever_is_pulled = digitalRead(lever_buttonPin) == LOW; //&& CUP_present && numDrinksTilEmpty > 0
-  if (lever_is_pulled) {
+  cup_is_present = digitalRead(cup_present_buttonPin) == LOW; // CUP_present && numDrinksTilEmpty > 0
+  if (cup_is_present) {
        // Initiate Mule Making:
        // show initialization animation
        digitalWrite(ledOrange_Vodka, LOW);
        digitalWrite(ledYellow_GingerBeer, LOW);
        digitalWrite(ledGreen_Lime, LOW);
-       delay(100);
+       delay(drink_starting_blink_duration);
        digitalWrite(ledOrange_Vodka, HIGH);
        digitalWrite(ledYellow_GingerBeer, HIGH);
        digitalWrite(ledGreen_Lime, HIGH);
-       delay(100);
+       delay(drink_starting_blink_duration);
        digitalWrite(ledOrange_Vodka, LOW);
        digitalWrite(ledYellow_GingerBeer, LOW);
        digitalWrite(ledGreen_Lime, LOW);
-       delay(100);
+       delay(drink_starting_blink_duration);
        digitalWrite(ledOrange_Vodka, HIGH);
        digitalWrite(ledYellow_GingerBeer, HIGH);
        digitalWrite(ledGreen_Lime, HIGH);
-       delay(100);
+       delay(drink_starting_blink_duration);
        digitalWrite(ledOrange_Vodka, LOW);
        digitalWrite(ledYellow_GingerBeer, LOW);
        digitalWrite(ledGreen_Lime, LOW);
-       delay(100);
+       delay(drink_starting_blink_duration);
        digitalWrite(ledOrange_Vodka, HIGH);
        digitalWrite(ledYellow_GingerBeer, HIGH);
        digitalWrite(ledGreen_Lime, HIGH);
-       delay(100);
+       delay(drink_starting_blink_duration);
        digitalWrite(ledOrange_Vodka, LOW);
        digitalWrite(ledYellow_GingerBeer, LOW);
        digitalWrite(ledGreen_Lime, LOW);
-       while(lever_is_pulled){
+       while(cup_is_present){
+
+           // UPDATE RECIPE
+           // 0.5 Cups per serving GB--> 2/3rd of Drink should be Ginger Beer
+           // 0.0625 Cups per serving Lime --> Of remaining 1/3, 1/4 should be Lime
+           // 0.1875 Cups per serving Vodka --> Of remaining 1/3, 3/4 should be Vodka
+           // -----------------------------------------------------------------------
+           // NOMINAL: GB 800/1200, Lime 100/1200, Vodka, 300/1200 --> Total time of 1200ms
+           //NOTE: Lower Limit of pump values must be at least current_spike_delay in value
+           value = analogRead(tuner_strength_pin);
+           calibration_ratio_Vodka = map(value, 0, 1023, 100 + current_spike_delay, 500);
+           value = analogRead(tuner_sour_pin);
+           calibration_ratio_Lime = map(value, 0, 1023, current_spike_delay, 200);
+           calibration_ratio_GingerBeer = 1200 - calibration_ratio_Vodka - calibration_ratio_Lime;
+           
            // MIX DRINK
+           // Start by switching all pumps to "ON". use a current_spike_delay to prevent the current from spiking
+           // and reseting the Arduino.
            digitalWrite(ledYellow_GingerBeer, HIGH);
            digitalWrite(pump_GingerBeer, HIGH);
-           delay(10);
+           delay(current_spike_delay);
            digitalWrite(ledOrange_Vodka, HIGH);
            digitalWrite(pump_Vodka, HIGH);
-           delay(10);           
+           delay(current_spike_delay);           
            digitalWrite(ledGreen_Lime, HIGH);
            digitalWrite(pump_Lime, HIGH);
 
-           delay((calibration_ratio_Lime * calibration_time_pour_duration));
-           digitalWrite(pump_Lime, LOW);
-           digitalWrite(ledGreen_Lime, LOW);
+           // turn off pumps in order of least time to greatest time
+           // NOTE: ginger beer will be the greatest time
+           if((calibration_ratio_Lime+current_spike_delay) < calibration_ratio_Vodka){
+             delay((calibration_ratio_Lime));
+             digitalWrite(pump_Lime, LOW);
+             digitalWrite(ledGreen_Lime, LOW);
 
-           delay((calibration_ratio_Vodka * calibration_time_pour_duration) - (calibration_ratio_Lime * calibration_time_pour_duration) - 10);
-           digitalWrite(pump_Vodka, LOW);
-           digitalWrite(ledOrange_Vodka, LOW);
+             delay((calibration_ratio_Vodka) - (calibration_ratio_Lime) - current_spike_delay);
+             digitalWrite(pump_Vodka, LOW);
+             digitalWrite(ledOrange_Vodka, LOW);
+             
+           } else {
+             delay(calibration_ratio_Vodka - current_spike_delay);
+             digitalWrite(pump_Vodka, LOW);
+             digitalWrite(ledOrange_Vodka, LOW);
 
-           delay((calibration_ratio_GingerBeer * calibration_time_pour_duration) - (calibration_ratio_Vodka * calibration_time_pour_duration) - (calibration_ratio_Lime * calibration_time_pour_duration) - 20);
+             delay((calibration_ratio_Lime) - (calibration_ratio_Vodka - current_spike_delay));
+             digitalWrite(pump_Lime, LOW);
+             digitalWrite(ledGreen_Lime, LOW);
+           }
+
+           delay(calibration_ratio_GingerBeer - calibration_ratio_Vodka - calibration_ratio_Lime - 2*current_spike_delay);
            digitalWrite(pump_GingerBeer, LOW);
            digitalWrite(ledYellow_GingerBeer, LOW);
                      
-           lever_is_pulled = digitalRead(lever_buttonPin) == LOW;
+           cup_is_present = digitalRead(cup_present_buttonPin) == LOW;
        }
        digitalWrite(ledOrange_Vodka, LOW);
        digitalWrite(ledYellow_GingerBeer, LOW);
        digitalWrite(ledGreen_Lime, LOW);
-  } 
-  //else if (pressed && No Cup present) {
-  // TODO: SHOW an ERROR (RED LED) next to Cup spot
-  //}
+  }
 }
